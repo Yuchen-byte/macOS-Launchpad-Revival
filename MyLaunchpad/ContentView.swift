f7a7e3f6b365e02ee22a304741404c5b0197668f
fanyuchen <2994114386@qq.com>
2026-03-25 16:27:36 +0800

    Initial Commit

---
+++ MyLaunchpad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyLaunchpad
+//
+//  Created by yuchen on 2026/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLaunchpad/MyLaunchpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLaunchpadApp.swift
+//  MyLaunchpad
+//
+//  Created by yuchen on 2026/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MyLaunchpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
